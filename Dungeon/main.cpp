ced05b517c3fcb0e29158f6b32cfab5af2deded4
csvenja <c.svenjax@gmail.com>
2014-07-16 23:27:48 -0700

    Initial Commit

---
+++ Dungeon/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Dungeon
+//
+//  Created by Svenja on 2014-07-16.
+//  Copyright (c) 2014 Shan Cao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
